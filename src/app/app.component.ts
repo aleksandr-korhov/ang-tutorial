fe2212413e62e773dff2e42c186ba787137c302c
aleksandr.korhov <aleksandr.korhov@devz.aitibiz.ru>
2017-08-12 15:59:34 +0300

    new tutorial 1 lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Hero } from "./shared/hero";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'Tour of Heroes';
+    // heroes = ['Batman', 'Spyderman', 'IronMan', 'Superman', 'Arrow', 'Grut', 'Rocket'];
+    heroes: Hero[] = [
+        new Hero(1, 'Batman'), 
+        new Hero(2, 'Spyderman'),
+        new Hero(3, 'IronMan'),
+        new Hero(4, 'Superman'),
+        new Hero(5, 'Arrow'),
+        new Hero(6, 'Grut'),
+        new Hero(7, 'Rocket')
+    ];
+    myHero = this.heroes[0];
 }
